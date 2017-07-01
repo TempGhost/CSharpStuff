03eb96050f03eb67a5791aa379d09d665cfa43ad
TempGhost <547628607@qq.com>
2017-07-02 02:10:42 +0800

    new file:   .gitattributes 	new file:   .gitignore 	modified:   MyPractice/.vs/MyPractice/v15/.suo 	modified:   MyPractice/MyPractice/Program.cs 	modified:   MyPractice/MyPractice/obj/Debug/CoreCompileInputs.cache 	modified:   MyPractice/MyPractice/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   MyPracticeCoreVersion/MyPracticeCoreVersion/MyPracticeCoreVersion.sln 	new file:   MyPracticeCoreVersion/MyPracticeCoreVersion/MyPracticeCoreVersion/Code/Document.cs 	new file:   MyPracticeCoreVersion/MyPracticeCoreVersion/MyPracticeCoreVersion/Code/DocumentManager.cs 	new file:   MyPracticeCoreVersion/MyPracticeCoreVersion/MyPracticeCoreVersion/Code/ProcessDocument.cs 	new file:   MyPracticeCoreVersion/MyPracticeCoreVersion/MyPracticeCoreVersion/Code/Racer.cs 	new file:   MyPracticeCoreVersion/MyPracticeCoreVersion/MyPracticeCoreVersion/Code/RegularExpressionsPractice.cs 	new file:   MyPracticeCoreVersion/MyPracticeCoreVersion/MyPracticeCoreVersion/MyPracticeCoreVersion.csproj 	new file:   MyPracticeCoreVersion/MyPracticeCoreVersion/MyPracticeCoreVersion/Program.cs

---
+++ MyPracticeCoreVersion/MyPracticeCoreVersion/MyPracticeCoreVersion/Code/DocumentManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPractice.Code
+{
+    class DocumentManager
+    { 
+         private  readonly  Queue<Document> documentQueue  = new Queue<Document>();
+
+        public void AddDocument(Document doc)
+        {
+            lock (this)
+            { 
+                documentQueue.Enqueue(doc); 
+            }
+        }
+
+        public Document GetDocument()
+        {
+            Document doc = null;
+            lock (this)
+            {
+                doc = documentQueue.Dequeue();
+            }
+            return doc;
+        }
+
+        public bool IsDocumentAvailable
+        {
+            get { return documentQueue.Count > 0; }
+        }
+    } 
+}
---
+++ MyPracticeCoreVersion/MyPracticeCoreVersion/MyPracticeCoreVersion/Code/Racer.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyPractice.Code
+{
+
+
+    public class FindCountry
+    {
+
+        private string Country;
+        public FindCountry(string Country)
+        {
+            this.Country = Country;
+        }
+
+        public bool FindCountryPredicate(Racer CurrRacer)
+        {
+            if (CurrRacer==null)
+            {
+               throw  new ArgumentNullException("未将对象引用到实例。");
+            }
+            return CurrRacer.Country == this.Country;
+        }
+    }  
+
+    class RacerTestProgram
+    {
+        public static void TestProgram()
+        {
+            List<Racer> RacerlList = new List<Racer>
+            {
+                new Racer(1, "I m so", "Dick", "China"),
+                new Racer(2, "I m so", "Suck", "Zimbabwe")
+            };  
+
+
+            RacerlList.ForEach(CurrenRacer => {
+                Console.WriteLine(CurrenRacer.ToString());
+            });
+
+            List<Racer> FindCountryResult = RacerlList.FindAll(new FindCountry("Zimbabwe").FindCountryPredicate);
+            FindCountryResult.ForEach(r => Console.WriteLine(r.ToString())); 
+
+          FindCountryResult = RacerlList.FindAll( new Predicate<Racer>(new FindCountry("Zimbabwe").FindCountryPredicate));
+           FindCountryResult.ForEach(r => Console.WriteLine(r.ToString())); 
+
+            Console.ReadLine();
+        } 
+    }
+   public  class Racer :IComparable<Racer>,IFormattable
+    {
+        public Racer(int id, string firstName, string lastName, string country)  //重载 构造函数
+            :this(id,firstName,lastName, country, 0)
+        {
+        }
+
+        public Racer(int id, string firstName, string lastName, string country, int wins) //定义构造 函数  
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Country = country;
+            Wins = wins;
+        }
+
+        public int Id { set; get; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Country { get; set; }
+
+        public int Wins { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", FirstName, LastName);
+        }
+
+        public string ToString(string format,IFormatProvider formatProvider)
+        {
+            if (format==null)
+            {
+                format = "N";
+            }
+            switch (format)
+            {
+                case "N":
+                    return ToString();
+                case "F":
+                    return FirstName;
+                case "L":
+                    return LastName;
+                case "W":
+                    return string.Format("{0} wins:{1}", ToString(), Wins);
+                case "C":
+                    return  string.Format("{0} Country:{1} ", ToString(), Country);
+                case "A":
+                    return     string.Format("{0} Country:{1} wins:{2}", ToString(), Country, Wins);
+                default:
+                    throw  new FormatException(string.Format(formatProvider,"Format {0} is not spported",format));
+            }
+        }
+
+        public string ToString(string format)
+        {
+            return ToString(format, null);
+        }
+
+        public int CompareTo(Racer Other)
+        {
+            if (Other == null)
+            {
+                return -1;
+            }
+            int compare = string.Compare(this.LastName,Other.LastName);
+            if (compare==0)
+            {
+                compare  =  string.Compare(this.FirstName, Other.FirstName);
+            }
+            return compare;
+        }
+    }
+
+
+
+}
