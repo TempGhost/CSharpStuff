64988a5c07fdf1d6e0912e2f3db88632fe7be73c
TempGhost <547628607@qq.com>
2017-07-21 00:07:51 +0800

    adddddddd

--- MyPracticeCoreVersion/MyPracticeCoreVersion/Program.cs
+++ MyPracticeCoreVersion/MyPracticeCoreVersion/Program.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using System.Dynamic;
 using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using DataLib;
 using MyPractice.Code;
 using MyPracticeCoreVersion.Code;
 using MyPracticeCoreVersion.Code.pipeline;
@@ -111,6 +113,7 @@ namespace MyPractice
             //     } 
             // }
             #endregion
+
             #region  字典类练习代码
 
             /**************************************************************************************************/
@@ -131,59 +134,147 @@ namespace MyPractice
             /**************************************************************************************************/
             #endregion
 
-
-
             #region 并发集合与管道练习代码
-            /*管道模型练习代码
-             代码实现功能：遍历制定目录文件夹 下所 指定文件
-             统计所有单词出现次数
-             用颜色标注 采用并发集合及并发编程中的管道模型*/
-            var fileNames = new BlockingCollection<string>(); //新建线程安全集合  存放 所用待分析的文件路径猜测使用悲观锁实现（悲观锁
-            //使用前总是认为会有其他线程来访问所有在访问前加锁）
-            var lines  = new BlockingCollection<string>(); //线程安全集合2 存放所有待分析文本
-             var words = new ConcurrentDictionary<string,int>(); //线程安全字典，存放分析集合2所得到的单词
-            var items = new BlockingCollection<Info>();//线程安全集合3 存放统计单词所得到的信息（总数、值、用何种颜色标识）
-            var colorItems = new BlockingCollection<Info>();//有点迷 * _ *
-            Task t1 = PipelineStages.ReadFilenamesAsync(@"C:\Git\CSharpStuff\CollectionsSamples", fileNames);
-            //创建线程t1 任务为读取指定目录及所有子目录的文件
-            ConsoleHelper.WriteLine("started stage 1 ");
-            Task t2 = PipelineStages.LoadContentAsync(fileNames,lines);
-            //创建线程2 任务为读取任务1的结果并将文件内容读取到集合2 
-            ConsoleHelper.WriteLine("stated stage 2");
-            Task t3 = PipelineStages.ProcessContentAsync(lines, words);
-            //创建线程3 任务为分析线程2的结果统计所有单词出现次数
-            ConsoleHelper.WriteLine("stated stage 3");
-             Task.WhenAll(t1, t2, t3).Wait();
+            ///*管道模型练习代码
+            // 代码实现功能：遍历制定目录文件夹 下所 指定文件
+            // 统计所有单词出现次数
+            // 用颜色标注 采用并发集合及并发编程中的管道模型*/
+            //var fileNames = new BlockingCollection<string>(); //新建线程安全集合  存放 所用待分析的文件路径猜测使用悲观锁实现（悲观锁
+            ////使用前总是认为会有其他线程来访问所有在访问前加锁）
+            //var lines  = new BlockingCollection<string>(); //线程安全集合2 存放所有待分析文本
+            // var words = new ConcurrentDictionary<string,int>(); //线程安全字典，存放分析集合2所得到的单词
+            //var items = new BlockingCollection<Info>();//线程安全集合3 存放统计单词所得到的信息（总数、值、用何种颜色标识）
+            //var colorItems = new BlockingCollection<Info>();//有点迷 * _ *
+            //Task t1 = PipelineStages.ReadFilenamesAsync(@"C:\Git\CSharpStuff\CollectionsSamples", fileNames);
+            ////创建线程t1 任务为读取指定目录及所有子目录的文件
+            //ConsoleHelper.WriteLine("started stage 1 ");
+            //Task t2 = PipelineStages.LoadContentAsync(fileNames,lines);
+            ////创建线程2 任务为读取任务1的结果并将文件内容读取到集合2 
+            //ConsoleHelper.WriteLine("stated stage 2");
+            //Task t3 = PipelineStages.ProcessContentAsync(lines, words);
+            ////创建线程3 任务为分析线程2的结果统计所有单词出现次数
+            //ConsoleHelper.WriteLine("stated stage 3");
+            // Task.WhenAll(t1, t2, t3).Wait();
 
-            #region 并发集合分析与总结
+            //#region 并发集合分析与总结
 
-            //主线程挂起等待同步
-            //疑问？ 为何同为何T1，T2，T3 同步执行 却能保证运行顺序？
-            //若T2 已经遍历完集合内所有元素，T1 遇到了一个文件需要10秒来完成 10秒以后T2 如何得知T1 又添加了一个新元素
-            //还是如果出现这种情况 T1 的部分文档会得不到处理？
-            //验证  在T1 中加入 Thread.Sleep  模式该情况 ，结果是好像T2 在轮询从T1的集合1一样，无论隔了多久
-            //只要T1 有新文档加入 T2 马上就在处理
-            //至此并发集合原理基本清晰 
-            //原来在使用并发集合Add 方法之后 并发集合的状态会处理正在加入元素的状态， 之后的代码访问该集合
-            //若该集合仍在正在加入元素的状态，则其实根本不能访问该集合的最后一个元素；并不会抛出异常，因为该集合的最后一个元素
-            //可能其实指向一个线程阻塞的函数，所有需要访问最后一个元素的代码都会在这里被挂起，等待。那么是什么才释放呢
-            //在调用该函数 CompleteAdding();的时候，在使用该方法之后 该集合处于完成添加元素的状态，所有访问该集合最后一个元素的
-            //请求被放行/释放，这就是为什么只要T1一有新元素加入T2 就可以马上处理的奥秘，其实T2 一直无法访问最后一个元素N 
-            //当T1 增加元素N+1 时 那么最后一个元素的指针指向N+1 此时N可以访问，在处理完N之后又会在N+1 处挂起 知道集合调用
-            //CompleteAdding()方法
-            //猜测  虽然T2 依赖与T1 的结果 但却不需要在T1 完全执行完才能运行，
+            ////主线程挂起等待同步
+            ////疑问？ 为何同为何T1，T2，T3 同步执行 却能保证运行顺序？
+            ////若T2 已经遍历完集合内所有元素，T1 遇到了一个文件需要10秒来完成 10秒以后T2 如何得知T1 又添加了一个新元素
+            ////还是如果出现这种情况 T1 的部分文档会得不到处理？
+            ////验证  在T1 中加入 Thread.Sleep  模式该情况 ，结果是好像T2 在轮询从T1的集合1一样，无论隔了多久
+            ////只要T1 有新文档加入 T2 马上就在处理
+            ////至此并发集合原理基本清晰 
+            ////原来在使用并发集合Add 方法之后 并发集合的状态会处理正在加入元素的状态， 之后的代码访问该集合
+            ////若该集合仍在正在加入元素的状态，则其实根本不能访问该集合的最后一个元素；并不会抛出异常，因为该集合的最后一个元素
+            ////可能其实指向一个线程阻塞的函数，所有需要访问最后一个元素的代码都会在这里被挂起，等待。那么是什么才释放呢
+            ////在调用该函数 CompleteAdding();的时候，在使用该方法之后 该集合处于完成添加元素的状态，所有访问该集合最后一个元素的
+            ////请求被放行/释放，这就是为什么只要T1一有新元素加入T2 就可以马上处理的奥秘，其实T2 一直无法访问最后一个元素N 
+            ////当T1 增加元素N+1 时 那么最后一个元素的指针指向N+1 此时N可以访问，在处理完N之后又会在N+1 处挂起 知道集合调用
+            ////CompleteAdding()方法
+            ////猜测  虽然T2 依赖与T1 的结果 但却不需要在T1 完全执行完才能运行，
+
+            //#endregion
+            //ConsoleHelper.WriteLine("1,2,3 completed");
+            //ConsoleHelper.WriteLine("stated stage 4");
+            //Task t4 = PipelineStages.TransferContentAsync(words, items);
+            //ConsoleHelper.WriteLine("stated stage 4");
+            //Task t5 = PipelineStages.AddColorAsync(items, colorItems);
+            //ConsoleHelper.WriteLine("stated stage 5");
+            //Task t6 = PipelineStages.ShowContentAsyncCollection(colorItems);
+            //Task.WaitAll(t4, t5, t6);
+            //ConsoleHelper.WriteLine("4,5,6 completed");
+            #endregion
+
+
+            #region linq表达式练习代码
+            //foreach (DataLib.Racer racer in DataLib.Formula1
+            //    .GetChampions().Where(c => c.Country == "Brazil")
+            //    .OrderByDescending(r => r.Wins))
+            //{
+            //     Console.WriteLine(string.Format("{0},{1}",racer.FirstName,racer.Country));
+            //}
+
+            //foreach (DataLib.Racer item in from s in DataLib.Formula1.GetChampions()
+            //    where s.Country == "Brazil" && s.Wins > 1
+            //    select s)
+            //{
+            //    Console.WriteLine(string.Format("{0},{1}", item.FirstName, item.Country));
+            //}
+
+            //var races = DataLib.Formula1.GetChampions().Where(
+            //    (r, index) =>
+            //    { 
+            //        Console.WriteLine(index);
+            //        return r.Country == "Brazil";
+            //    });
+            //foreach (var item in races)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //foreach (var itemRacer in from s in DataLib.Formula1.GetChampions() 
+            //    from c in s.Cars where c == "Frrari"
+            //    select s)
+            //{
+            //}
+            var races2 = from r in Formula1.GetChampions() //linq 表达式 查询返回新的匿名对象集合
+                from y in r.Years //from 子句 当对象的属性本身又是集合 使用from子句可降维
+                select new //如二维对象  racer: {name: jack ,championyear :[2017,2016,2015]}
+                {
+                    //可降为  name:jack ,championyear:2017 
+                    Year = y, // name:jack,championyear:2016
+                    Name = r.FirstName //name:jack,championyear:2015 三条记录
+                };
+            var teams = from t in Formula1.GetContructorChampions() //作用同上
+                from y in t.Years
+                select new
+                {
+                    Year = y,
+                    Name = t.Name,
+                };
+             //linq   join 查询
+             //和将两个集合通过某种个字段/属性连接起来
+             //返回一个新的匿名对象集合
+            var racersAndTeams = (from r in races2  
+                join t in teams on r.Year equals t.Year
+                select new {r.Year, Champion = r.Name, Constructor = t.Name}).OrderBy(
+                x => x.Year).Take(10);
+            Console.WriteLine("Year Wrold Champion\t Constructor Title");
+            foreach (var item in racersAndTeams)
+            {
+                Console.WriteLine("{0} :{1,-20} ,{2}", item.Year, item.Champion, item.Constructor);
+            }
+             
+            Console.WriteLine("--------------------------------------------------------------------------------------");
+
+            //合并join 语句版本
+            var racersAndTeamsNew = (
+                from r in from r1 in Formula1.GetChampions() //这里开始
+                from yr in //这里是为了将对象降维
+                r1.Years
+                select new
+                {
+                    Year = yr,
+                    Name = r1.FirstName
+                }  //这里结束 是集合一
+                join t in from t1 in Formula1.GetContructorChampions() // 这里开始
+                from yr in t1.Years //同样 降维
+                select new
+                {
+                    Year = yr,
+                    Name = t1.Name
+                }//这里结束 是集合2
+                on r.Year equals t.Year //连接凭据 func <t1,t2,bool>
+                select new {Year = r.Year, Racer = r.Name, Team = t.Name}).Take(10);
+
+            foreach (var item in racersAndTeamsNew)
+            {
+                Console.WriteLine("{0} :{1,-20} ,{2}", item.Year, item.Racer, item.Team);
+            }
 
             #endregion
-            ConsoleHelper.WriteLine("1,2,3 completed");
-            ConsoleHelper.WriteLine("stated stage 4");
-            Task t4 = PipelineStages.TransferContentAsync(words, items);
-            ConsoleHelper.WriteLine("stated stage 4");
-            Task t5 = PipelineStages.AddColorAsync(items, colorItems);
-            ConsoleHelper.WriteLine("stated stage 5");
-            Task t6 = PipelineStages.ShowContentAsyncCollection(colorItems);
-            Task.WaitAll(t4, t5, t6);
-            ConsoleHelper.WriteLine("4,5,6 completed");
-            #endregion
+
             Console.ReadLine();
         }
 
